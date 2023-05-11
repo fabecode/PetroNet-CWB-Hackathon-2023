49f65406867df52a381c67e058d80f265cd1f86e
Fabrianne <fab.effendi@gmail.com>
2023-05-11 17:23:32 +0800

    Initial commit of Azure Function

---
+++ topic-modelling/TopicModellingAzureFunction/__init__.py
@@ -0,0 +1,23 @@
+import logging
+import azure.functions as func
+
+#APP_URL = "http://localhost:7071/api/TopicModellingAzureFunction"
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+            name = req_body.get('name')
+        except ValueError:
+            pass
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
